e5201bd7fb6df5fd8020634d7b32a2fc353ec7a8
impostr <anubhav1661@gmail.com>
2018-08-20 19:45:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 
 module.exports = {
-  consumer_key: 'xGKRObOrPLi8UWA1G45fjL0BP',
-  consumer_secret: 'MMx06JVfypspEOtZ3PEIfflMwAUa2u4gtm56jwaSmXP6Xc4OFZ',
-  access_token_key: '928597420044251136-RmLy8kC6fat3TI7g1TSEvut7cWQTGo8',
-  access_token_secret: 'zwWiAkyWqqBP7pgR3jeiaEunIKwqaMcc0R9ciLtWUt2wx'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 }
